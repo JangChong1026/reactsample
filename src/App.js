c45d481c757132cd1a5d6531d070d33959a38881
JangChong1026 <aliaksei93127@gmail.com>
2021-08-21 22:06:33 +0300

    header part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Home from './components/home';
+import Header from "./components/Header"
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home/>
     </div>
   );
